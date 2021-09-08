fd5f1dadb4b868d3d0210abeab8a529d02760c58
AlexRussovits <sasikrus2002@gmail.com>
2021-09-08 11:28:01 +0300

    Game

--- src/game/Game.java
+++ src/game/Game.java
@@ -5,6 +5,9 @@
  */
 package game;
 
+import java.util.Random;
+import java.util.Scanner;
+
 /**
  *
  * @author pupil
@@ -14,8 +17,17 @@ public class Game {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public static void main(String[] args) {
+        Random random = new Random ();
+        Scanner scanner = new Scanner(System.in);
+        int number = random.nextInt(9-0+1)+0;
+        System.out.println("Pogramma zapomnila chisla ot 0 do 9, ugadaj");
+        int userNumber = scanner.nextInt();
+        if(number == userNumber){
+            System.out.println("Ura! Tq vqigral!!");
+        }else{
+            System.out.println(":(,nepravilno. Zadumano chislo: "+number);
+        }
     }
     
 }
